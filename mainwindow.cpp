79c4b4dd09aefcd1ad512a92b31a6e3832ace1e2
Alexandra Eftimie <alexandraeftimie@Alexandras-MBP.lan>
2020-06-12 13:19:30 -0700

    changed welcome buttons color

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setStyleSheet("background-color: black;");
+
 }
 
 MainWindow::~MainWindow()
