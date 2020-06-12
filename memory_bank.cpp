abd505dedd2fb944d84f552414924702e019c3e4
Alexandra Eftimie <alexandraeftimie@Alexandras-MBP.lan>
2020-06-12 16:13:06 -0700

    member function defined

--- memory_bank.cpp
+++ memory_bank.cpp
@@ -1,3 +1,11 @@
 #include "memory_bank.h"
 
+Memory_Bank::Memory_Bank() : mems(std::vector<Memory>()) {}
 
+void Memory_Bank::log_memory(Memory m){
+    mems.push_back(m);
+}
+
+Memory Memory_Bank::get_memory(int index){
+    return mems[index];
+}
