c0a1e08e5b35daf71bd3ec0306f4bbab09e49f14
Alexandra Eftimie <alexandraeftimie@Alexandras-MBP.lan>
2020-06-12 14:57:12 -0700

    fix

--- memory_bank.h
+++ memory_bank.h
@@ -1,11 +1,17 @@
 #ifndef MEMORY_BANK_H
 #define MEMORY_BANK_H
 
+#include "memory.h"
+#include <vector>
 
-class memory_bank
+class Memory_Bank
 {
+private:
+    std::vector<Memory> mems;
 public:
-    memory_bank();
+    Memory_Bank();
+    void log_memory(Memory);
+    Memory get_memory(int);
 };
 
 #endif // MEMORY_BANK_H
